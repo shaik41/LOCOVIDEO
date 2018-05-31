e4cd658f38ca4dec67848a6fa643392c56eb0720
Shaik Atif <shaikatif@Atifs-Macbook.local>
2018-05-31 14:43:43 +0530

    Removed used code

--- app/src/main/java/com/task/locovideotask/ui/videoutils/VideoUtils.java
+++ app/src/main/java/com/task/locovideotask/ui/videoutils/VideoUtils.java
@@ -3,7 +3,6 @@ package com.task.locovideotask.ui.videoutils;
 import android.util.DisplayMetrics;
 
 
-import com.task.locovideotask.model.ViewData;
 
 import java.util.Random;
 
@@ -13,22 +12,6 @@ import java.util.Random;
 
 public class VideoUtils {
 
-    public static ViewData adjustAspectRatio(int proposedWidth, int proposedHeight, int videoWidth, int videoHeight) {
-
-        float ratio_width = proposedWidth/videoWidth;
-        float ratio_height = proposedHeight/videoHeight;
-        float aspect_ratio = videoWidth/videoHeight;
-        ViewData viewData=new ViewData();
-        if (ratio_width > ratio_height){
-            viewData.setWidth((int) (proposedHeight * aspect_ratio));
-            viewData.setHeight(proposedHeight);
-        }else{
-            viewData.setWidth(proposedWidth);
-            viewData.setHeight((int) (proposedWidth/aspect_ratio));
-        }
-
-        return viewData;
-    }
 
     public static int getPixelsFromDPI(int size, DisplayMetrics displayMetrics){
         return (size * displayMetrics.densityDpi) / DisplayMetrics.DENSITY_DEFAULT;
